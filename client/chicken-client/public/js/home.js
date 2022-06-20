9b24aff9bcd0588b5054785a2e7faa44ec17bf07
Thomas Peugnet <thomas.peugnet.pro@gmail.com>
2022-06-21 00:24:32 +0200

    Adding hoz scroll and continue servers and mp areas

--- client/chicken-client/public/js/home.js
+++ client/chicken-client/public/js/home.js
@@ -1,1 +1,22 @@
 console.log(localStorage.getItem("token"));
+
+enable_horizontal_scroll();
+
+handle_right_click();
+
+function handle_right_click() {
+    document.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+        alert("Right click test");
+    }
+        , false);
+}
+
+
+function enable_horizontal_scroll() {
+    const scrollContainer = document.querySelector(".servers_container");
+    scrollContainer.addEventListener("wheel", (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+}
